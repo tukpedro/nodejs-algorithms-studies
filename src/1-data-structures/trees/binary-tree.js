c37c7dc48a953d30cd4c4048c2a24c099092ec41
Pedro Pastuk <pastukpedro@gmail.com>
2023-10-13 14:05:56 -0300

    feat: :art: binary trees

--- src/1-data-structures/trees/binary-tree.js
+++ src/1-data-structures/trees/binary-tree.js
@@ -1,19 +1,25 @@
 // Define a Node class for the binary tree
 class Node {
+	/**
+	 * Constructor for the Node class.
+	 * @param {number} data - The data for the node.
+	 */
 	constructor(data) {
-		this.data = data; // Data contained in the node
-		this.left = null; // Pointer to the left child
-		this.right = null; // Pointer to the right child
+		this.data = data;
+		this.left = null;
+		this.right = null;
 	}
 }
 
-// Define the BinaryTree class
 module.exports = class BinaryTree {
 	constructor() {
 		this.root = null; // Root node of the tree
 	}
 
-	// Method to insert data into the binary tree
+	/**
+	 * Inserts a new node with the given data into the tree.
+	 * @param {number} data - The data for the new node.
+	 */
 	insert(data) {
 		const newNode = new Node(data);
 		if (this.root === null) {
@@ -23,20 +29,105 @@ module.exports = class BinaryTree {
 		}
 	}
 
-	// Helper function to insert a node in the binary tree
+	/**
+	 * Recursive helper function to insert a node in the binary tree.
+	 * @param {Node} node - The current node.
+	 * @param {Node} newNode - The new node to insert.
+	 */
 	insertNode(node, newNode) {
 		if (newNode.data < node.data) {
 			if (node.left === null) {
 				node.left = newNode; // If left child is empty, insert here
 			} else {
-				this.insertNode(node.left, newNode); // Otherwise, move to the left child and repeat
+				this.insertNode(node.left, newNode);
 			}
 		} else {
 			if (node.right === null) {
 				node.right = newNode; // If right child is empty, insert here
 			} else {
-				this.insertNode(node.right, newNode); // Otherwise, move to the right child and repeat
+				this.insertNode(node.right, newNode);
 			}
 		}
+	}
+
+	/**
+	 * Searches for a node with the given data in the tree.
+	 * @param {number} data - The data to search for.
+	 * @returns {Node|null} - The node with the given data or null if not found.
+	 */
+	search(data) {
+		return this.searchNode(this.root, data);
+	}
+
+	/**
+	 * Recursive helper function for the search method.
+	 * @param {Node} node - The current node.
+	 * @param {number} data - The data to search for.
+	 * @returns {Node|null} - The node with the given data or null if not found.
+	 */
+	searchNode(node, data) {
+		if (node === null) return null;
+
+		if (data < node.data) {
+			return this.searchNode(node.left, data);
+		} else if (data > node.data) {
+			return this.searchNode(node.right, data);
+		} else {
+			return node;
+		}
+	}
+
+	/**
+	 * Removes a node with the given data from the tree.
+	 * @param {number} data - The data of the node to remove.
+	 */
+	remove(data) {
+		this.root = this.removeNode(this.root, data);
+	}
+
+	/**
+	 * Recursive helper function for the remove method.
+	 * @param {Node} node - The current node.
+	 * @param {number} data - The data of the node to remove.
+	 * @returns {Node|null} - The updated node after removal.
+	 */
+	removeNode(node, data) {
+		if (node === null) return null;
+
+		if (data < node.data) {
+			node.left = this.removeNode(node.left, data);
+			return node;
+		} else if (data > node.data) {
+			node.right = this.removeNode(node.right, data);
+			return node;
+		} else {
+			// Case 1: node with no children
+			if (node.left === null && node.right === null) {
+				node = null;
+				return node;
+			}
+
+			// Case 2: node with one child
+			if (node.left === null) return node.right;
+			if (node.right === null) return node.left;
+
+			// Case 3: node with two children
+			let tempNode = this.findMinNode(node.right);
+			node.data = tempNode.data;
+			node.right = this.removeNode(node.right, tempNode.data);
+			return node;
+		}
+	}
+
+	/**
+	 * Finds the node with the smallest value greater than the given node.
+	 * @param {Node} node - The starting node.
+	 * @returns {Node} - The node with the minimum value.
+	 */
+	findMinNode(node) {
+		while (node && node.left !== null) {
+			node = node.left;
+		}
+		return node;
 	}
 };
